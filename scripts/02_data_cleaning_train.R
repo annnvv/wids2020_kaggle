7eb33a393ffff780f0a739305818d278fec21937
annnvv <13822260+annnvv@users.noreply.github.com>
2020-02-01 00:51:33 -0500

    updated train cleaning script

--- scripts/02_data_cleaning_train.R
+++ scripts/02_data_cleaning_train.R
@@ -4,134 +4,144 @@
   
   proj_path <- "C:/Users/Anna V/Documents/GitHub/wids2020_kaggle"
   
-  train <- read.csv(paste0(proj_path, "/data/training.csv"))
+  raw_train <- read.csv(paste0(proj_path, "/raw_data/training.csv"))
   
-  train$apache_2_bodysystem[train$apache_2_bodysystem == "Undefined Diagnoses"] <- "Undefined diagnoses"
-  table(train$apache_2_bodysystem)
-  train$apache_2_bodysystem <- droplevels(train$apache_2_bodysystem)
-  
-  # test <- read.csv(paste0(proj_path, "/data/unlabeled.csv"))
-  # # create train/test identifier
-  # train$cat <- "train"
-  # test$cat <- "test"
-  # table(test$hospital_death) #make sure this is empty
-  # #append all of the rows together
-  # full <- rbind(train, test)
-  # rm(train, test)
-  
-  # can drop "patient_id" and "encounter_id" as explantory variables, they are unique!
-  train <- train[ , -which(names(train) %in% c("encounter_id", "patient_id"))]
+  raw_train$apache_2_bodysystem[raw_train$apache_2_bodysystem == "Undefined Diagnoses"] <- "Undefined diagnoses"
+  table(raw_train$apache_2_bodysystem)
+  raw_train$apache_2_bodysystem <- droplevels(raw_train$apache_2_bodysystem)
+
+  # can drop "patient_id" as explantory variables, they are unique!
+  # need "encounter_id"
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c("patient_id"))] #"hosp_id", "icu_id" 
   # remove variables with zero variance
-  train <- train[ - as.numeric(which(apply(train, 2, var) == 0))] #only one var, readmit_status
+  raw_train <- raw_train[ - as.numeric(which(apply(raw_train, 2, var) == 0))] #only one var, readmit_status
   
   # drop variables with invasive in their name! (They have a lot of missing data)
-  inv <- names(train)[grep("_invasive_", names(train))]
-  train <- train[ , -which(names(train) %in% c(inv))]
+  inv <- names(raw_train)[grep("_invasive_", names(raw_train))]
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c(inv))]
   ninety <- c("h1_albumin_max", "h1_albumin_min", "h1_bilirubin_max", "h1_bilirubin_min", "h1_lactate_max", "h1_lactate_min")
-  train <- train[ , -which(names(train) %in% c(ninety))]
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c(ninety))]
   rm(inv, ninety)
   
   # factor variables
-  factor_df <- train[ , sapply(train,is.factor)]
+  factor_df <- raw_train[ , sapply(raw_train,is.factor)]
   names(factor_df)
   
-  train <- fastDummies::dummy_cols(train, select_columns=names(factor_df)[c(1, 3:8)], 
+  raw_train <- fastDummies::dummy_cols(raw_train, select_columns=names(factor_df)[c(1, 3:8)], 
                                    ignore_na = TRUE, remove_selected_columns = TRUE)
-  names(train) <- gsub("_source_", "_src_", names(train))
-  names(train) <- gsub("hospital", "hosp", names(train))
-  names(train) <- gsub("-", "_", names(train))
-  names(train) <- gsub(" ", "_", names(train))
-  names(train) <- tolower(names(train))
+  rm(factor_df)
   
-  names(train)[165:length(train)]
+  names(raw_train) <- gsub("_source_", "_src_", names(raw_train))
+  names(raw_train) <- gsub("hospital", "hosp", names(raw_train))
+  names(raw_train) <- gsub("-", "_", names(raw_train))
+  names(raw_train) <- gsub(" ", "_", names(raw_train))
+  names(raw_train) <- tolower(names(raw_train))
+  
+  names(raw_train)[165:length(raw_train)]
   
   #drop variables that end with an underscore (i.e. missing data)
-  miss <- names(train)[grep("_$", names(train))]
+  miss <- names(raw_train)[grep("_$", names(raw_train))]
+  miss
   miss <- miss[2:5]
-  train <- train[ , -which(names(train) %in% c(miss))]
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c(miss))]
+  rm(miss)
   
-  
-  # for (i in 165:length(train)){
-  #   print(names(train[i]))
-  #   print(mean(train[i], na.rm = TRUE))
-  #   print(table(is.na(train[i])))
-  #   
+  # for (i in 165:length(raw_train)){
+  #   print(names(raw_train[i]))
+  #   print(mean(raw_train[i], na.rm = TRUE))
+  #   print(table(is.na(raw_train[i])))
   # }
   
-  # change hostpital_id and icu_id to factors!
-  train$hosp_id <- as.factor(train$hosp_id)
-  train$icu_id <- as.factor(train$icu_id)
+  # change hostpital_id and icu_id to characters!
+  raw_train$hosp_id <- as.character(raw_train$hosp_id)
+  raw_train$icu_id <- as.character(raw_train$icu_id)
   
   #deal with missing data!
   #### demographic data
-  hist(train$age) #replace with mean
+  hist(raw_train$age) #replace with mean
+  raw_train$age[is.na(raw_train$age)] <- mean(raw_train$age, na.rm = TRUE)
+  prop.table(table(is.na(raw_train$age)))*100
   
-  train$age[is.na(train$age)] <- mean(train$age, na.rm = TRUE)
+  hist(raw_train$bmi) #replace with median
+    tapply(raw_train$bmi, raw_train$gender, median, na.rm = TRUE)
+  raw_train$bmi[is.na(raw_train$bmi)&raw_train$gender == ""] <- tapply(raw_train$bmi, raw_train$gender, median, na.rm = TRUE)[1]
+  raw_train$bmi[is.na(raw_train$bmi)&raw_train$gender == "F"] <- tapply(raw_train$bmi, raw_train$gender, median, na.rm = TRUE)[2]
+  raw_train$bmi[is.na(raw_train$bmi)&raw_train$gender == "M"] <- tapply(raw_train$bmi, raw_train$gender, median, na.rm = TRUE)[3]
+  prop.table(table(is.na(raw_train$bmi)))*100
   
-  hist(train$bmi) #replace with median
-  # tapply(train$bmi, train$gender, median, na.rm = TRUE)
-  train$bmi[is.na(train$bmi)&train$gender == "F"] <- tapply(train$bmi, train$gender, median, na.rm = TRUE)[2]
-  train$bmi[is.na(train$bmi)&train$gender == "M"] <- tapply(train$bmi, train$gender, median, na.rm = TRUE)[3]
+  hist(raw_train$height) #replace with median
+  # mean(raw_train$height, na.rm = TRUE)
+  tapply(raw_train$height, raw_train$gender, median, na.rm = TRUE) # women tend to be shorter
+  raw_train$height[is.na(raw_train$height)&raw_train$gender == ""] <- tapply(raw_train$height, raw_train$gender, median, na.rm = TRUE)[1]
+  raw_train$height[is.na(raw_train$height)&raw_train$gender == "F"] <- tapply(raw_train$height, raw_train$gender, median, na.rm = TRUE)[2]
+  raw_train$height[is.na(raw_train$height)&raw_train$gender == "M"] <- tapply(raw_train$height, raw_train$gender, median, na.rm = TRUE)[3]
+  prop.table(table(is.na(raw_train$height)))*100
   
-  hist(train$height) #replace with median
-  # mean(train$height, na.rm = TRUE)
-  # tapply(train$height, train$gender, mean, na.rm = TRUE) # women tend to be shorter
-  train$height[is.na(train$height)&train$gender == "F"] <- tapply(train$height, train$gender, mean, na.rm = TRUE)[2]
-  train$height[is.na(train$height)&train$gender == "M"] <- tapply(train$height, train$gender, mean, na.rm = TRUE)[3]
+  hist(raw_train$weight) #replace with median
+    tapply(raw_train$weight, raw_train$gender, mean, na.rm = TRUE) # men tend to weight more
+  raw_train$weight[is.na(raw_train$weight)&raw_train$gender == ""] <- tapply(raw_train$weight, raw_train$gender, median, na.rm = TRUE)[1]
+  raw_train$weight[is.na(raw_train$weight)&raw_train$gender == "F"] <- tapply(raw_train$weight, raw_train$gender, median, na.rm = TRUE)[2]
+  raw_train$weight[is.na(raw_train$weight)&raw_train$gender == "M"] <- tapply(raw_train$weight, raw_train$gender, median, na.rm = TRUE)[3]
+  prop.table(table(is.na(raw_train$weight)))*100
   
-  hist(train$weight) #replace with median
-  # tapply(train$weight, train$gender, mean, na.rm = TRUE) # men tend to weight more
-  train$weight[is.na(train$weight)&train$gender == "F"] <- tapply(train$weight, train$gender, median, na.rm = TRUE)[2]
-  train$weight[is.na(train$weight)&train$gender == "M"] <- tapply(train$weight, train$gender, median, na.rm = TRUE)[3]
-  
-  # need to do something with ethnicity and gender
+  # need to do something with gender
+  table(raw_train$gender) # based on averages of missing above, they are more similar to men
+  prop.table(table(raw_train$gender))*100
+  raw_train$gender[raw_train$gender == ""] <- "M"
+  raw_train$gender <- droplevels(raw_train$gender)
   
   # dealing with numeric data!
   with_miss <- c()
-  for (i in names(train)){
-    if (sum(is.na(train[ , i]))/nrow(train)>0){
+  for (i in names(raw_train)){
+    if (sum(is.na(raw_train[ , i]))/nrow(raw_train)>0){
       with_miss <- c(with_miss, i)
     }
   }
   rm(i)
   
-  train2 <- train
-  
   for (i in with_miss){
-    train[is.na(train2[i]), i] <- mean(train2[ , i], na.rm = TRUE)
+    raw_train[is.na(raw_train[i]), i] <- mean(raw_train[ , i], na.rm = TRUE)
   }
+  rm(i, with_miss)
+  #raw_train[is.na(raw_train["bmi"]), "bmi"] <- mean(raw_train[ , "bmi"], na.rm = TRUE)
   
-  train[is.na(train2["bmi"]), "bmi"] <- mean(train2[ , "bmi"], na.rm = TRUE)
+  complete <- raw_train[complete.cases(raw_train), ] ## complete cases, hurrah!
+  rm(complete)
   
+  #re-order to have hosp_death as first var!
+  raw_train <- raw_train[ , c(1, 3, 2, 4:length(raw_train))]
   
-  View(train)
+  #drop this factor variable because it doesn't exist in the test set!
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c("hosp_admit_src_observation"))]
+  
+  #remove death probability rates from data
+  raw_train <- raw_train[ , -which(names(raw_train) %in% c("apache_4a_hosp_death_prob", "apache_4a_icu_death_prob" ))]
+  
+  write.csv(raw_train, paste0(proj_path, "/clean_data/train_cleaned.csv"), row.names = FALSE)
 
-  
-  form2 <- as.formula("hosp_death ~ age + weight + height + gender + elective_surgery + leukemia + ethnicity_caucasian")
-  
-  lm2 <- lm(form2, data = train)
-  summary(lm2)
-  
-  glm2 <- glm(form2, data = train, family = "binomial")
-  summary(glm2)
+  # form1 <- as.formula("hosp_death ~ .")
+  # lm1 <- lm(form1, data = raw_train)
+  # summary(lm1)
+    
+  # form2 <- as.formula("hosp_death ~ age + weight + height + gender + elective_surgery + leukemia + ethnicity_caucasian")
+  # lm2 <- lm(form2, data = raw_train)
+  # summary(lm2)
+  # 
+  # glm2 <- glm(form2, data = raw_train, family = "binomial")
+  # summary(glm2)
 
-  
-  form1 <- as.formula("hosp_death ~ .")
-  
-  lm1 <- lm(form1, data = train)
-  summary(lm1)
-  
+
   # ###lasso 
   # library(glmnet)
-  # cv_output <- cv.glmnet(x_vars[train,], y_var[train], 
+  # cv_output <- cv.glmnet(x_vars[raw_train,], y_var[raw_train], 
   #                        alpha = 1, lambda = lambda_seq)
   
-  # tapply(train$pre_icu_los_days, train$hospital_death, mean)
-  # tapply(train$albumin_apache, train$hospital_death, mean, na.rm = TRUE)
+  # tapply(raw_train$pre_icu_los_days, raw_train$hospital_death, mean)
+  # tapply(raw_train$albumin_apache, raw_train$hospital_death, mean, na.rm = TRUE)
   # replace_miss <- function(var){
-  #   train[var][is.na(train[var])&train$hospital_death == 0] <- tapply(train[,var], train$hospital_death, mean, na.rm = TRUE)[1] #survived
-  #   train[var][is.na(train[var])&train$hospital_death == 1] <- tapply(train[,var], train$hospital_death, mean, na.rm = TRUE)[2] #died
+  #   raw_train[var][is.na(raw_train[var])&raw_train$hospital_death == 0] <- tapply(raw_train[,var], raw_train$hospital_death, mean, na.rm = TRUE)[1] #survived
+  #   raw_train[var][is.na(raw_train[var])&raw_train$hospital_death == 1] <- tapply(raw_train[,var], raw_train$hospital_death, mean, na.rm = TRUE)[2] #died
   # }
-  # train$albumin_apache <- replace_miss("albumin_apache")
+  # raw_train$albumin_apache <- replace_miss("albumin_apache")
   ## can't use the function above because test data does not have the target variable!
   
