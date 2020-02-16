da22b70497134fdae998571b7e55c95b7fa1b0db
annnvv <13822260+annnvv@users.noreply.github.com>
2020-02-15 22:57:59 -0500

    subset train and test based on feature selection

--- scripts/04_feature_selection.R
+++ scripts/04_feature_selection.R
@@ -1,4 +1,11 @@
   # Feature Selection
+  #https://beta.vu.nl/nl/Images/werkstuk-fonti_tcm235-836234.pdf
+
+  rm(list = ls())
+  
+  library(glmnet)
+  library(caret)
+  
   proj_path <- "C:/Users/Anna V/Documents/GitHub/wids2020_kaggle"
   
   train_all <- read.csv(paste0(proj_path, "/clean_data/train_cleaned.csv"), stringsAsFactors = FALSE)
@@ -29,4 +36,42 @@
   
   rm(both)
 
-    
+
+  #https://stackoverflow.com/questions/21858124/r-error-in-glmnet-na-nan-inf-in-foreign-function-call
+  x <- model.matrix( ~ -1 + ., train_all[ ,c(3:length(train_all))])
+  
+  #### LASSO Feature Selection ####
+  fit <- cv.glmnet(x, as.factor(train_all$hosp_death), 
+                family = "binomial", alpha=1, folds = 5, measure = "auc")
+  plot(fit)
+  coef(fit)
+  print(fit)
+  
+  #code based on: https://stats.stackexchange.com/questions/58531/using-lasso-from-lars-or-glmnet-package-in-r-for-variable-selection
+  c <- coef(fit, extract = TRUE)
+  inds <- which(c!=0)
+  variables <- row.names(c)[inds]
+  `%ni%` <- Negate(`%in%`)
+  variables <-variables[variables %ni% '(Intercept)']
+  
+  variables <- variables[!grepl("^hosp_id", variables)]
+  variables <- variables[!grepl("^icu_id", variables)]
+  variables <- c("hosp_id", "icu_id", variables)  
+
+  variables  
+  rm(c, inds)
+  
+  # subset and write clean csvs
+  train_all_sub <- train_all[ , variables]
+  write.csv(train_all_sub, paste0(proj_path, "/clean_data/train_cleaned_sub.csv"), 
+            row.names = FALSE)
+  
+  test_all_sub <- test_all[ , variables]
+  write.csv(test_all_sub, paste0(proj_path, "/clean_data/test_cleaned_sub.csv"), row.names = FALSE)
+  
+  # for (i in 1:length(train_all)){
+  #   if (table(is.na(train_all[,i]))){
+  #     print(names(train_all[, i]))
+  #   }
+  # }
+  
